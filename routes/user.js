15cdcf4b9e1bbd3638e657a9fbc8fc770bc3c4ed
cornacchia <pozzan.gabriele@gmail.com>
2017-10-10 14:26:38 +0200

    Add basic report page template

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,18 @@ var router = express.Router()
 
 /* GET user home page. */
 router.get('/', function(req, res, next) {
-  res.render('report', { title: 'Nuovo rapportino' })
+  var db = database.get()
+  
+    db.collection('Misc').find({}).toArray(function (err, result) {
+      var elements = {
+        'vehicle': [],
+        'site': []
+      }
+      for (var i in result) {
+        elements[result[i].category].push(result[i])
+      }
+      return res.render('report', { title: 'Nuovo rapportino', elements: elements })
+    })
 })
 
 module.exports = router
