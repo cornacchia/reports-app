28a447a6b492d4509b579e89b11d8a2eab229f48
cornacchia <pozzan.gabriele@gmail.com>
2018-01-30 10:55:34 +0100

    Add bootstrap datepicker module

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-rm -f public/stylesheets/bootstrap.min.css.map
-rm -f public/stylesheets/bootstrap.min.css
-rm -f public/javascripts/bootstrap.min.js
-rm -f public/javascripts/jquery.min.js
+./clean.sh
 
 cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/stylesheets
 cp node_modules/bootstrap/dist/css/bootstrap.min.css public/stylesheets/
 cp node_modules/bootstrap/dist/js/bootstrap.min.js public/javascripts
 cp node_modules/jquery/dist/jquery.min.js public/javascripts
+cp node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css public/stylesheets
+cp node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js public/javascripts
--- clean.sh
+++ clean.sh
@@ -4,3 +4,5 @@ rm -f public/stylesheets/bootstrap.min.css.map
 rm -f public/stylesheets/bootstrap.min.css
 rm -f public/javascripts/bootstrap.min.js
 rm -f public/javascripts/jquery.min.js
+rm -f public/stylesheets/bootstrap-datepicker.min.css
+rm -f public/javascripts/bootstrap-datepicker.min.js
