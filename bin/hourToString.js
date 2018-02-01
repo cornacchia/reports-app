e8ba323ad23c714885e26772e55b9e0fb8e82db4
cornacchia <pozzan.gabriele@gmail.com>
2018-02-01 10:27:35 +0100

    Remove trailing console.log

--- bin/hourToString.js
+++ bin/hourToString.js
@@ -1,7 +1,6 @@
 /** Converts a full date to dayily hours */
 
 module.exports = function hourToString (d) {
-  console.log(d)
   const date = new Date(d)
   return date.getHours() + ':' +
     date.getMinutes() + ':' +
